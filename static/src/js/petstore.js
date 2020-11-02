b794498d85bc7b73ae67ce1c1832e508904367d9
Ange Mollou <angemollou@outlook.com>
2020-11-02 01:09:09 +0000

    Basics

--- static/src/js/petstore.js
+++ static/src/js/petstore.js
@@ -1,42 +1,54 @@
 odoo.define("petstore.petstore", function (require) {
   "use strict";
 
-  var Class = require('web.Class');
-  var Widget = require('web.Widget');
-  var core = require('web.core');
-  var utils = require('web.utils');
+  var Class = require("web.Class");
+  var Widget = require("web.Widget");
+  var core = require("web.core");
+  var utils = require("web.utils");
+  var qweb = core.qweb;
 
   var _t = core._t;
   var _lt = core._lt;
 
   var homePage = Widget.extend({
-    className: "oe_petstore_homepage",
-    init: function(parent) {
-      this._super(parent);
-      console.log('Hello from the init');
-      alert('Hello from the init');
-    },
+    className: "petstore_homepage",
     start: function () {
-      console.log("pet store home page loaded");
-      alert("pet store home page loaded");
+      this.$el.append(
+        qweb.render("HomePageTemplate", {
+          a: "GREAT",
+          c: "black",
+        })
+      );
 
-      this.$el.append("<div>Hello dear Odoo!</div>");
+      /******************
+       Before Using QWeb 
+      *******************/
+      var greeting = new GreetingsWidget(this, "Bob");
+      // var greeting = new GreetingsWidget(null);
+      // greeting.destroy();
 
-      var greeting = new GreetingsWidget(this);
+      if (typeof this.getChildren()[0] != "undefined") {
+        console.log("children: " + this.getChildren()[0].$el);
+      }
+
       return greeting.appendTo(this.$el);
     },
   });
 
   var GreetingsWidget = Widget.extend({
-    className: "oe_petstore_greetings",
+    template: "GreetingTemplate",
+    className: "petstore_greetings",
+    init: function (parent, name) {
+      this._super(parent);
+      this.name = name;
+    },
     start: function () {
-      this.$el.append(
-        "<div>We are so happy to see you again in this menu!</div>"
-      );
+      if (typeof this.getParent() != "undefined") {
+        console.log("parent: " + this.getParent().$el);
+      }
     },
   });
 
-  core.action_registry.add('petstore.homepage', homePage);
+  core.action_registry.add("petstore.homepage", homePage);
   core.action_registry.add("instance.petstore.HomePage", () => {});
-
 });
